53864f4015da51efb6dd214bcd6feebaaa1c7487
Daniel Fernandes Vieira <danielvieiraufc@gmail.com>
2021-12-23 10:40:33 -0300

    feat: create and style header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Api from './api/Api';
 import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import SignIn from './Components/SignIn/SignIn';
 
 function App() {
   return (
     <div>
-      <Api />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign_in" element={<SignIn />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
