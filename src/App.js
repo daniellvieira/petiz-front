3b24136e34ee9cda715a3ad65c3b7199fed5c83e
Daniel Fernandes Vieira <danielvieiraufc@gmail.com>
2022-01-02 21:31:02 -0300

    feat: create user profile page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Home from './Components/Home';
 import Login from './Components/Login/Login';
 import Photo from './Components/Photo/Photo';
 import User from './Components/User/User';
+import UserProfile from './Components/User/UserProfile';
 import { UserStorage } from './Contexts/User';
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="login/*" element={<Login />} />
             <Route path="photo/:id" element={<Photo />} />
+            <Route path="profile/:user" element={<UserProfile />} />
             <Route
               path="account/*"
               element={
