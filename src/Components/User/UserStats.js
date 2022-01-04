05801a9586d3807dd47b8b139bd897666ac50f9f
Daniel Fernandes Vieira <danielvieiraufc@gmail.com>
2022-01-04 17:56:17 -0300

    feat: calculate number total of access per user

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { STATS_GET } from '../../api';
 import Head from '../Helpers/Head';
+import useFetch from '../Hooks/useFetch';
+import Loading from '../Helpers/Loading';
+import Error from '../Helpers/Error';
+import UserStatsGraphs from './UserStatsGraphs';
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-      Estatisticas
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  else return null;
 };
 
 export default UserStats;
